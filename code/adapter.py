83b9a48d608fee33612ef58b71def944336518f4
moonorange <monoma632@gmail.com>
2020-08-19 11:32:14 +0900

    add: adapter python code

---
+++ code/adapter.py
@@ -0,0 +1,35 @@
+from abc import ABCMeta, abstractmethod
+
+def main():
+    pb = PrintBanner("Hello")
+    pb.print_weak()
+    pb.print_strong()
+
+class Banner:
+    def __init__(self, string):
+        self.string = string
+
+    def show_with_paren(self):
+        print("({})".format(self.string))
+
+    def show_with_aster(self):
+        print("*{}*".format(self.string))
+
+class Print(metaclass=ABCMeta):
+    @abstractmethod
+    def print_weak(self):
+        pass
+
+    @abstractmethod
+    def print_strong(self):
+        pass
+
+class PrintBanner(Banner, Print):
+    def print_weak(self):
+        self.show_with_paren()
+
+    def print_strong(self):
+        self.show_with_aster()
+
+if __name__ == '__main__':
+    main()
